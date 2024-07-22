09cfe1f7f3be65850bc0f4656e903633a67e4cd6
taeyoonahn <atyoon526@naver.com>
2024-07-22 23:29:55 +0900

    20240722_make_screen

---
+++ lib/screens/profileDetail/ProfileDetailScreen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import '../../widgets/profileDetail/ProfileDetailWidget.dart';
+
+class ProfileDetailScreen extends StatelessWidget {
+  const ProfileDetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ProfileDetailWidget();
+  }
+}
