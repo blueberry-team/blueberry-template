2a29bb4808a77b58ed23218f7e73d774c7443b04
Jungwoo <108061510+jwson-automation@users.noreply.github.com>
2024-07-25 22:50:05 +0900

    upd userModel

--- lib/model/UserModel.dart
+++ lib/model/UserModel.dart
@@ -1,6 +1,7 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'generated/UserModel.freezed.dart';
+
 part 'generated/UserModel.g.dart';
 
 // TODO :  User Class 추가
@@ -8,13 +9,16 @@ part 'generated/UserModel.g.dart';
 @freezed
 class UserModel with _$UserModel {
   const factory UserModel({
+    required String userClass,
     required String userId,
     required String name,
     required String email,
     required int age,
     required String profileImageUrl,
     required DateTime createdAt,
+    required String fcmToken,
   }) = _UserModel;
 
-  factory UserModel.fromJson(Map<String, dynamic> json) => _$UserModelFromJson(json);
+  factory UserModel.fromJson(Map<String, dynamic> json) =>
+      _$UserModelFromJson(json);
 }
