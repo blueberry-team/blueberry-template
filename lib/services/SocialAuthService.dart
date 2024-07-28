43e20d607c14a25159856dbb9182215886ab9bb1
kimwest00 <kimwest00@hufs.ac.kr>
2024-07-28 12:00:52 +0900

    fix(login,google):authService 네이밍 구체화,주석추가, 구글 key,redirectUrl 추가및 null 조건 체크 로직 추가

--- lib/services/SocialAuthService.dart
+++ lib/services/SocialAuthService.dart
@@ -3,35 +3,23 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 
-class AuthService {
+class SocialAuthService {
   get context => null;
 
-
   ///Google Sign In
-  signInWithGoogle() async {
-    final GoogleSignInAccount? gUser = await GoogleSignIn().signIn();
-
-    final GoogleSignInAuthentication gAuth = await gUser!.authentication;
-
-    final credential = GoogleAuthProvider.credential(
-      accessToken: gAuth.accessToken,
-      idToken: gAuth.idToken,
-    );
-
-    await FirebaseAuth.instance.signInWithCredential(credential);
-    var ref = FirebaseFirestore.instance.collection('users').doc(FirebaseAuth.instance.currentUser!.uid);
-    var snapshot = await ref.get();
-    if(!snapshot.exists){
-      return await FirebaseFirestore.instance.collection('users').doc(FirebaseAuth.instance.currentUser!.uid).set({
-        'account_level' : 1,
-        //account_level이 0이되면 Delete timestamp확인하여 14일 뒤 삭제
-        'email' : FirebaseAuth.instance.currentUser!.email,
-        'name' : FirebaseAuth.instance.currentUser!.displayName,
-        'age' : 0,
-        'createdAt' : DateTime.timestamp(),
-        'profilePicture' : "",
-      }
+  Future<void> signInWithGoogle() async {
+    ///* GoogleSignIn 객체 생성
+    final googleSignIn = GoogleSignIn();
+    final GoogleSignInAccount? gUser = await googleSignIn.signIn();
+    if (gUser != null) {
+      final GoogleSignInAuthentication gAuth = await gUser.authentication;
+      final gCredential = GoogleAuthProvider.credential(
+        accessToken: gAuth.accessToken,
+        idToken: gAuth.idToken,
       );
+      final userCredential =
+          await FirebaseAuth.instance.signInWithCredential(gCredential);
+      getAuthenticateWithFirebase(userCredential);
     }
   }
 
@@ -49,24 +37,28 @@ class AuthService {
       accessToken: appleIdCredential.authorizationCode,
     );
     await FirebaseAuth.instance.signInWithCredential(credential);
-    var ref = FirebaseFirestore.instance.collection('users').doc(FirebaseAuth.instance.currentUser!.uid);
+    var ref = FirebaseFirestore.instance
+        .collection('users')
+        .doc(FirebaseAuth.instance.currentUser!.uid);
     var snapshot = await ref.get();
-    if(!snapshot.exists){
-      return await FirebaseFirestore.instance.collection('users').doc(FirebaseAuth.instance.currentUser!.uid).set({
-        'account_level' : 1,
+    if (!snapshot.exists) {
+      return await FirebaseFirestore.instance
+          .collection('users')
+          .doc(FirebaseAuth.instance.currentUser!.uid)
+          .set({
+        'account_level': 1,
         //account_level이 0이되면 Delete timestamp확인하여 14일 뒤 삭제
-        'email' : FirebaseAuth.instance.currentUser!.email,
-        'name' : FirebaseAuth.instance.currentUser!.displayName,
-        'age' : 0,
-        'createdAt' : DateTime.timestamp(),
-        'profilePicture' : "",
-      }
-      );
+        'email': FirebaseAuth.instance.currentUser!.email,
+        'name': FirebaseAuth.instance.currentUser!.displayName,
+        'age': 0,
+        'createdAt': DateTime.timestamp(),
+        'profilePicture': "",
+      });
     }
   }
 
   ///Github Sign In
-  Future<UserCredential>signInWithGithub() async{
+  Future<UserCredential> signInWithGithub() async {
     GithubAuthProvider githubAuthProvider = GithubAuthProvider();
     return await FirebaseAuth.instance.signInWithProvider(githubAuthProvider);
     // scopes: [
@@ -90,9 +82,30 @@ class AuthService {
     //   }
     //   );
     // }
-
   }
 
-
+  ///* 인증정보를 바탕으로 firestore에 저장하는 함수
+  void getAuthenticateWithFirebase(UserCredential? credential) async {
+    if (credential?.user == null) {
+      return null;
+    }
+    var ref = FirebaseFirestore.instance
+        .collection('users')
+        .doc(FirebaseAuth.instance.currentUser!.uid);
+    var snapshot = await ref.get();
+    if (!snapshot.exists) {
+      return await FirebaseFirestore.instance
+          .collection('users')
+          .doc(FirebaseAuth.instance.currentUser!.uid)
+          .set({
+        'account_level': 1,
+        //account_level이 0이되면 Delete timestamp확인하여 14일 뒤 삭제
+        'email': FirebaseAuth.instance.currentUser!.email,
+        'name': FirebaseAuth.instance.currentUser!.displayName,
+        'age': 0,
+        'createdAt': DateTime.timestamp(),
+        'profilePicture': "",
+      });
+    }
+  }
 }
-
