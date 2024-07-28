5627011e7deccb5f884e601337c455eb0f27c9c5
kimwest00 <kimwest00@hufs.ac.kr>
2024-07-28 12:43:37 +0900

    fix(login,github): github 중복 코드 삭제, ios 세팅추가(README 추가 필요)

--- lib/services/SocialAuthService.dart
+++ lib/services/SocialAuthService.dart
@@ -58,30 +58,12 @@ class SocialAuthService {
   }
 
   ///Github Sign In
-  Future<UserCredential> signInWithGithub() async {
+  Future<void> signInWithGithub() async {
     GithubAuthProvider githubAuthProvider = GithubAuthProvider();
-    return await FirebaseAuth.instance.signInWithProvider(githubAuthProvider);
-    // scopes: [
-    //   displayName : userCredential.user!.displayName!,
-    //   photoURL: userCredential.user!.photoURL ?? "",
-    //   email: userCredential.user!.email!,
-    // ],
+    final userCredential =
+        await FirebaseAuth.instance.signInWithProvider(githubAuthProvider);
 
-    // await FirebaseAuth.instance.signInWithCredential(credential);
-    // var ref = FirebaseFirestore.instance.collection('users').doc(FirebaseAuth.instance.currentUser!.uid);
-    // var snapshot = await ref.get();
-    // if(!snapshot.exists){
-    //   return await FirebaseFirestore.instance.collection('users').doc(FirebaseAuth.instance.currentUser!.uid).set({
-    //     'account_level' : 1,
-    //     //account_level이 0이되면 Delete timestamp확인하여 14일 뒤 삭제
-    //     'email' : FirebaseAuth.instance.currentUser!.email,
-    //     'name' : FirebaseAuth.instance.currentUser!.displayName,
-    //     'age' : 0,
-    //     'createdAt' : DateTime.timestamp(),
-    //     'profilePicture' : "",
-    //   }
-    //   );
-    // }
+    getAuthenticateWithFirebase(userCredential);
   }
 
   ///* 인증정보를 바탕으로 firestore에 저장하는 함수
