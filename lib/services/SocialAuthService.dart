fb3c0247b5f7f26b16ff7628f612c1ec07db2245
kimwest00 <kimwest00@hufs.ac.kr>
2024-08-04 02:18:32 +0900

    fix(login,apple):onError 메소드 추가, 키 생성 로직 추가(필요없다면 추후 제거)

--- lib/services/SocialAuthService.dart
+++ lib/services/SocialAuthService.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+import 'dart:math';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:crypto/crypto.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 
@@ -24,38 +29,42 @@ class SocialAuthService {
   }
 
   ///Apple Sign In
-  signInWithApple() async {
-    final appleIdCredential = await SignInWithApple.getAppleIDCredential(
-      scopes: [
-        AppleIDAuthorizationScopes.email,
-        AppleIDAuthorizationScopes.fullName,
-      ],
-    );
-    final oAuthProvider = OAuthProvider('apple.com');
-    final credential = oAuthProvider.credential(
+  void signInWithApple() async {
+    final rawNonce = generateNonce();
+    final nonce = sha256ofString(rawNonce);
+
+    late final AuthorizationCredentialAppleID appleIdCredential;
+    await SignInWithApple.getAppleIDCredential(scopes: [
+      AppleIDAuthorizationScopes.email,
+      AppleIDAuthorizationScopes.fullName,
+    ], nonce: nonce)
+        .then((value) {
+      appleIdCredential = value;
+    }).onError((error, stackTrace) {
+      if (error is PlatformException) return;
+    });
+
+    final credential = OAuthProvider('apple.com').credential(
       idToken: appleIdCredential.identityToken,
       accessToken: appleIdCredential.authorizationCode,
+      rawNonce: rawNonce,
     );
-    await FirebaseAuth.instance.signInWithCredential(credential);
-    var ref = FirebaseFirestore.instance
-        .collection('users')
-        .doc(FirebaseAuth.instance.currentUser!.uid);
-    var snapshot = await ref.get();
-    if (!snapshot.exists) {
-      return await FirebaseFirestore.instance
-          .collection('users')
-          .doc(FirebaseAuth.instance.currentUser!.uid)
-          .set({
-        'account_level': 1,
-        //account_level이 0이되면 Delete timestamp확인하여 14일 뒤 삭제
-        'email': FirebaseAuth.instance.currentUser!.email,
-        'name': FirebaseAuth.instance.currentUser!.displayName,
-        'age': 0,
-        'isMemberShip': false,
-        'createdAt': DateTime.timestamp(),
-        'profilePicture': "",
-      });
-    }
+    var result = await FirebaseAuth.instance.signInWithCredential(credential);
+    getAuthenticateWithFirebase(result);
+  }
+
+  String generateNonce([int length = 32]) {
+    final charset =
+        '0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._';
+    final random = Random.secure();
+    return List.generate(length, (_) => charset[random.nextInt(charset.length)])
+        .join();
+  }
+
+  String sha256ofString(String input) {
+    final bytes = utf8.encode(input);
+    final digest = sha256.convert(bytes);
+    return digest.toString();
   }
 
   ///Github Sign In
@@ -63,7 +72,6 @@ class SocialAuthService {
     GithubAuthProvider githubAuthProvider = GithubAuthProvider();
     final userCredential =
         await FirebaseAuth.instance.signInWithProvider(githubAuthProvider);
-
     getAuthenticateWithFirebase(userCredential);
   }
 
@@ -86,7 +94,6 @@ class SocialAuthService {
         'email': FirebaseAuth.instance.currentUser!.email,
         'name': FirebaseAuth.instance.currentUser!.displayName,
         'age': 0,
-        'isMemberShip': false,
         'createdAt': DateTime.timestamp(),
         'profilePicture': "",
       });
