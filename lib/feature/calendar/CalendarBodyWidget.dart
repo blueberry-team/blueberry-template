31f3f3aeeabb2eaae374afe7a46c63f53a9eaf32
kimwest00 <kimwest00@hufs.ac.kr>
2024-08-09 13:27:46 +0900

    feat: calendar body UI 코드 작성, 날짜 계산 로직 추가(추후 provider 분리)

--- lib/feature/calendar/CalendarBodyWidget.dart
+++ lib/feature/calendar/CalendarBodyWidget.dart
@@ -1,10 +1,66 @@
 part of 'CalendarScreen.dart';
 
-class CalendarBodyWidget extends StatelessWidget {
-  const CalendarBodyWidget({super.key});
+class CalendarBodyWidget extends StatefulWidget {
+  CalendarBodyWidget({super.key});
+
+  @override
+  State<CalendarBodyWidget> createState() => _CalendarBodyWidgetState();
+}
+
+class _CalendarBodyWidgetState extends State<CalendarBodyWidget> {
+  List<DateTime> dateTime = [];
+  final List<String> date = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"];
+
+  //TODO: 해당 로직 provider로 분리 예정
+  void setDate(int month, int year) {
+    final lastDay = DateTime(year, month + 1, 0);
+    final firstDay = DateTime(year, month, 1);
+    final int totalDate = lastDay.difference(firstDay).inDays;
+    dateTime = List.generate(
+        totalDate, (index) => firstDay.add(Duration(days: index)));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setDate(DateTime.now().month, DateTime.now().year);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        Row(
+          children: List.generate(
+            DateTime.daysPerWeek,
+            (idx) => _dateItem(date[idx]),
+          ),
+        ),
+        ...List.generate(
+          //TODO: 1일의 요일에 따른 배치 변경
+          dateTime.length,
+          (idx) => _dateItem(dateTime[idx].day.toString()),
+        ),
+      ],
+    );
+  }
+
+  Widget _dateItem(String item) {
+    final color = Color(0xFF5E5E5E);
+    final textStyle = TextStyle(
+      fontSize: 14.sp,
+      height: 16 / 14,
+      fontWeight: FontWeight.w500,
+      color: color,
+      //fontFamily: 'Urbanist',
+    );
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 14.w, horizontal: 5.4.w),
+      width: 50.29.w,
+      child: Text(
+        item,
+        style: textStyle,
+      ),
+    );
   }
 }
