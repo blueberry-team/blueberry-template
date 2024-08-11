490977343405c6274de5d88ca0ade7d547175cbf
Jungwoo <108061510+jwson-automation@users.noreply.github.com>
2024-08-11 11:59:59 +0900

    add talker

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,44 @@
+import 'dart:async';
+
 import 'package:blueberry_flutter_template/router/RouterProvider.dart';
 import 'package:blueberry_flutter_template/services/notification/firebase_cloud_messaging_manager.dart';
 import 'package:blueberry_flutter_template/utils/AppStrings.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import 'firebase_options.dart';
-import 'core/provider/ThemeProvider.dart';
-import 'utils/AppTheme.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
+import 'core/provider/ThemeProvider.dart';
+import 'firebase_options.dart';
+import 'utils/AppTheme.dart';
+import 'utils/Talker.dart';
+
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await initializeDateFormatting('en_US', null);
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  runZonedGuarded(() async {
+    // 날짜 형식 초기화
+    await initializeDateFormatting('en_US', null);
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
 
-  FirebaseCloudMessagingManager.initialize(onTokenRefresh: (token) {
-    debugPrint('FCM Token: $token');
+    FirebaseCloudMessagingManager.initialize(onTokenRefresh: (token) {
+      talker.info('FCM Token: $token');
+    });
+
+    // Crashlytics 설정
+    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+    if (!kIsWeb) {
+      FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
+    }
+
+    runApp(const ProviderScope(
+      child: MyApp(),
+    ));
+  }, (error, stackTrace) {
+    FirebaseCrashlytics.instance.recordError(error, stackTrace);
   });
-
-  runApp(const ProviderScope(
-    child: MyApp(),
-  ));
 }
 
 class MyApp extends StatelessWidget {
