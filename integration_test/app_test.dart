f145bf48ab41dd35430745bc86ed7a65d3cb51d2
suojae3 <suojae3@gmail.com>
2024-08-14 22:42:47 +0900

    :white_check_mark: test: 베이스클래스 삭제

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -5,8 +5,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
-import 'base_test.dart';
-
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
   late FirebaseFirestore firestore;
@@ -18,32 +16,28 @@ void main() {
   });
 
   group('Firestore에서 데이터를 가져와 fromJson 메서드 실행', () {
-    final testCase = BaseTestCase();
     late String collectionName;
 
     test('pet 컬렉션이 있을 때, 첫 번째 문서를 가져와 fromJson 메서드로 매핑하면, 오류 없이 매핑된다',
         () async {
       DogProfileModel? fetchedData;
 
-      testCase.given(() {
-        collectionName = 'pet';
-      });
+      // Given: 컬렉션 이름 설정
+      collectionName = 'pet';
 
-      testCase.when(() async {
-        final querySnapshot =
-            await firestore.collection(collectionName).limit(1).get();
+      // When: 첫 번째 문서 가져오기 및 fromJson 매핑
+      final querySnapshot =
+          await firestore.collection(collectionName).limit(1).get();
 
-        if (querySnapshot.docs.isNotEmpty) {
-          final data = querySnapshot.docs.first.data();
-          fetchedData = DogProfileModel.fromJson(data);
-        } else {
-          throw Exception('$collectionName 컬렉션 안에 다큐먼트가 존재하지 않습니다');
-        }
-      });
+      if (querySnapshot.docs.isNotEmpty) {
+        final data = querySnapshot.docs.first.data();
+        fetchedData = DogProfileModel.fromJson(data);
+      } else {
+        throw Exception('$collectionName 컬렉션 안에 다큐먼트가 존재하지 않습니다');
+      }
 
-      testCase.then(() {
-        expect(fetchedData, isNotNull, reason: 'fetchedData는 null이 아니어야 합니다.');
-      });
+      // Then: 매핑된 데이터 검증
+      expect(fetchedData, isNotNull, reason: 'fetchedData는 null이 아니어야 합니다.');
     });
   });
 }
