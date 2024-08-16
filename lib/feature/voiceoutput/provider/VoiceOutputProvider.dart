d40107b33917ab7180249ff8215f3899c58557fa
MKim <mrn.kim07@gmail.com>
2024-08-16 20:38:12 +0900

    Collection 및 field명 변경

--- lib/feature/voiceoutput/provider/VoiceOutputProvider.dart
+++ lib/feature/voiceoutput/provider/VoiceOutputProvider.dart
@@ -3,9 +3,10 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final voiceOutputProvider = StreamProvider<List<String>>((ref) {
   final firestore = FirebaseFirestore.instance;
-  return firestore.collection('textInputs').snapshots().map((snapshot) {
-    final textInputs =
-    snapshot.docs.map((doc) => doc['textInput'] as String).toList();
-    return textInputs;
+
+  return firestore.collection('voiceOutputs').snapshots().map((snapshot) {
+    final voiceOutputs =
+        snapshot.docs.map((doc) => doc['voiceOutput'] as String).toList();
+    return voiceOutputs;
   });
 });
