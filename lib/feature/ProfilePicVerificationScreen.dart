25dd3b7f7bf422ba2dc83e22a22d4f0b4954346e
JerraldKim <hyunho0302@hotmail.com>
2024-08-18 13:57:09 +0900

    사진첩 및 카메라에서 이미지 가져오는 기능과 프로필 사진 승인 후 디스플레이하는 기능을 추가

---
+++ lib/feature/ProfilePicVerificationScreen.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'ImageImporter.dart';
+
+class ProfilePicVerificationScreen extends ConsumerWidget {
+  const ProfilePicVerificationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final imageFile = ref.watch(imageFileProvider);
+
+    // 승인 여부
+    const bool isAuthorized = false; // true면 승인, false면 미승인
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('프로필 사진 페이지'),
+      ),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          double squareSize = constraints.maxWidth * 0.8;
+
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 100,
+                ),
+                Container(
+                  width: squareSize,
+                  height: squareSize,
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(20.0),
+                    border: Border.all(
+                      color: Colors.grey,
+                      width: 4.0,
+                    ),
+                  ),
+                  child: imageFile != null
+                      ? ClipRRect(
+                    borderRadius: BorderRadius.circular(20.0),
+                    child: Stack(
+                      children: [
+                        Image.file(
+                          imageFile,
+                          fit: BoxFit.cover,
+                          width: squareSize,
+                          height: squareSize,
+                          color: isAuthorized
+                              ? null
+                              : Colors.black.withOpacity(0.6),
+                          colorBlendMode:
+                          isAuthorized ? null : BlendMode.darken,
+                        ),
+                        if (!isAuthorized)
+                          Center(
+                            child: Text(
+                              '승인 대기 중입니다 \n조금만 기다려주세요',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                                backgroundColor:
+                                Colors.black.withOpacity(0.5),
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                      ],
+                    ),
+                  )
+                      : const Center(
+                    child: Icon(
+                      Icons.pets,
+                      size: 200,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    _showImageSourceActionSheet(context, ref);
+                  },
+                  child: const Text(
+                    '사진 수정',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w900),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  void _showImageSourceActionSheet(BuildContext context, WidgetRef ref) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return SafeArea(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ListTile(
+                leading: const Icon(Icons.photo_library),
+                title: const Text('갤러리에서 선택'),
+                onTap: () async {
+                  await ref
+                      .read(imageFileProvider.notifier)
+                      .pickImageFromGallery(context);
+                  Navigator.of(context).pop();
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.camera_alt),
+                title: const Text('카메라로 찍기'),
+                onTap: () async {
+                  await ref
+                      .read(imageFileProvider.notifier)
+                      .pickImageFromCamera(context);
+                  Navigator.of(context).pop();
+                },
+              ),
+              if (ref.read(imageFileProvider) != null)
+                ListTile(
+                  leading: const Icon(Icons.delete),
+                  title: const Text('사진 삭제'),
+                  onTap: () {
+                    ref.read(imageFileProvider.notifier).clearImage();
+                    Navigator.of(context).pop();
+                  },
+                ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
