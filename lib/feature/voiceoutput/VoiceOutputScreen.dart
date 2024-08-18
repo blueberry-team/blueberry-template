a8d5a97723431b525a410bbe305991e92ecc2cca
SangHyun Kim <vison901@gmail.com>
2024-08-18 17:56:23 +0900

    make 커맨드 실행 포멧팅

--- lib/feature/voiceoutput/VoiceOutputScreen.dart
+++ lib/feature/voiceoutput/VoiceOutputScreen.dart
@@ -1,22 +1,20 @@
-/*
-* 구현 중입니다.
-* 샘플 voiceOutput들을 각각 플레이 버튼과 함께 한 줄에 보여줍니다.
-* 8월 16일 미란
-* */
-
-import './widget/VoiceOutputListWidget.dart';
+import './widget/VoiceOutputWidget.dart';
 import 'package:flutter/material.dart';
 
 class VoiceOutputScreen extends StatelessWidget {
-  static const String name = 'VoiceOutput';
-
   const VoiceOutputScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: VoiceOutputListWidget(),
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(child: VoiceOutputWidget()),
+          ],
+        ),
       ),
     );
   }
